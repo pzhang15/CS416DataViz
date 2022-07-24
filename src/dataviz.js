8fb6f5b10af64b1f87cedb750b8cf8b8ebc9ab3f
Patrick Zhang <pzhang15@yahoo.com>
2022-07-24 15:22:56 -0400

    Update dataviz.js

--- src/dataviz.js
+++ src/dataviz.js
@@ -1,7 +1,7 @@
 var width = 500
 var height = 1500
 
-d3.csv("./data/GDPC1.csv", function(data) {
+d3.csv("data/GDPC1.csv", function(data) {
         for (var i = 0; i < data.length; i++) {
             console.log(data[i].DATE);
             console.log(data[i].GDPC1);
